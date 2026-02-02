f4725e8040e85624c5e7320492d6bd06a53897ad
SHAIK MOHAMMED ASAD <hi2mdasad@gmail.com>
2026-02-02 23:46:45 +0530

    Add initial code with bitwise operations

---
+++ b5.py
@@ -0,0 +1,15 @@
+>>> a=4
+>>> a<<2
+16
+>>>  v
+...  
+SyntaxError: unexpected indent
+>>> v
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    v
+NameError: name 'v' is not defined
+>>> a=5
+>>> a>>2
+1
+>>> 
