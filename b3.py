53eaec78379cbe17679c7e61c4f24df177400b17
SHAIK MOHAMMED ASAD <hi2mdasad@gmail.com>
2026-02-02 23:45:13 +0530

    Update b3.py

---
+++ b3.py
@@ -0,0 +1,9 @@
+Enter "help" below or click "Help" above for more information.
+>>> a=4
+>>> ~a
+-5
+>>> -(x+1)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    -(x+1)
+NameError: name 'x' is not defined
