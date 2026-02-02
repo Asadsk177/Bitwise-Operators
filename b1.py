0af6246ede6882ce90071bda31cfecd7052f980f
SHAIK MOHAMMED ASAD <hi2mdasad@gmail.com>
2026-02-02 15:00:28 +0530

    Update b1.py

---
+++ b1.py
@@ -0,0 +1,19 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #bitwise
+>>> #,&,|,~,^,<<,>>
+>>> a=3
+>>> b=2
+>>> &b
+SyntaxError: invalid syntax
+>>> a&b
+2
+>>> bin()
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    bin()
+TypeError: bin() takes exactly one argument (0 given)
+>>> bin(4)
+'0b100'
+>>> bin(3)
+'0b11'
